76e9a20fe74a234b2513038c8b2ca86f8079e336
Saqin Noor <saqin@saqinnoor.dpdns.org>
2026-06-25 07:48:25 +0600

    performance: code-split vendors, remove dead assets, non-blocking fonts

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,14 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three'],
+          gsap: ['gsap', 'gsap/ScrollTrigger'],
+        },
+      },
+    },
+  },
 })
